142cab73a7b235c52b2175a84bfaab45ef87e07d
Justin Abercrombia <jabercrombia@gmail.com>
2025-02-06 00:04:13 -0600

    remove font awesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import "./globals.css";
 import { Roboto } from 'next/font/google'
 import { EXAMPLE_PATH, CMS_NAME } from "@/lib/constants";
 
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false;
-
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Head from 'next/head'
