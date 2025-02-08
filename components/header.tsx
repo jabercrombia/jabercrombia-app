0a6e62ee527c6d2ad3e4ba32232e1fd67f4023b8
Justin Abercrombia <jabercrombia@gmail.com>
2025-02-08 00:04:35 -0600

    navigation

--- components/header.tsx
+++ components/header.tsx
@@ -12,17 +12,17 @@ function Header() {
     <header className="bg-black text-white p-[15px] text-center w-full">
         <div className="container mx-auto flex items-center">
           <Logo/>
-          <div className="uppercase text-xs">
+          <nav className="uppercase">
           {navigationArray.map(
             (elem, index, array) => (
-              <div className={index + 1 === array.length ? 'mr-4 pr-4 inline-block tracking-[3px]' : 'mr-4 border-r pr-4 inline-block tracking-[3px]'} key={index}>
+              <div className={index + 1 === array.length ? 'mr-4 inline-block' : 'mr-4 border-r pr-4 inline-block'} key={index}>
                 <Link href={`/`+elem} className="hover:text-cloudGray">
                   <p>{elem}</p>
                 </Link>
               </div>
             )
           )}
-          </div>
+          </nav>
         </div>
     </header>
   )
