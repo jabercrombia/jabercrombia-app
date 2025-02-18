5d57255f7f30dd38f0cd6db96c63c7c7cc7d06ca
Justin Abercrombia <jabercrombia@gmail.com>
2025-02-18 00:13:32 -0600

    project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -22,16 +22,15 @@ export default async function PostPage({
         {projects?.map(
           (elem: { title: string; description: string, url: string, photosCollection: { items: { url: string, title: string }[] } }, index: number) => (
             <div className="flex flex-wrap" key={index}>
-              <div className="md:w-1/2 w-full content-center p-[10px]">
+              <div className={index % 2 == 0 ? "md:w-1/2 w-full content-center p-[10px] order-1" : "order-2 md:w-1/2 w-full content-center p-[10px] md:text-right"}>
                 <h2>{elem.title}</h2>
-                <p>{elem.description}</p>
-                <VisitButton bg={"black"} btnText={"Visit"} classes="" textColor={"white"} linkDestination={elem.url} linkDestinationExternal={true} buttonLocation={index % 2 !== 0}/>
+                <p className="text-sm">{elem.description}</p>
+                <VisitButton bg={"black"} btnText={"Visit"} classes="md:order-3" textColor={"white"} linkDestination={elem.url} linkDestinationExternal={true} buttonLocation={index % 2 !== 0}/>
               </div>
-              <div className="md:w-1/2 w-full  p-[10px]">
+              <div className={index % 2 == 0 ? "md:w-1/2 w-full p-[10px]" : "md:w-1/2 w-full p-[10px] order-1 md:order-2"}>
                 <img src={elem.photosCollection.items[0].url} alt={elem.photosCollection.items[0].title} className="border-2 border-grey p-2"/>
               </div>
             </div>
-
           )
         )}
         </div>
