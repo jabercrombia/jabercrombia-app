cb5217384f56b472645b7728a74b3a2894eee092
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-04-15 14:21:43 -0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,29 @@
+const fetchSlugs = async () => {
+  const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`);
+  const data = await res.json();
+
+  console.log(data.fields);
+  return data.items.map((item) => {
+    const slug = item.fields.slug;
+    const contentType = item.sys.contentType.sys.id; // Assuming the content type ID is stored here
+    if (!['blogPost', 'technology', 'about','techNames','pageHeader'].includes(contentType)) {
+
+      return {
+        loc: `${contentType}/${slug}`,
+        lastmod: new Date(item.sys.updatedAt).toISOString(),
+      };
+
+    }
+
+  });
+};
+
+
+module.exports = {
+  siteUrl: process.env.SITE_URL, // Your website URL
+  generateRobotsTxt: true, // Optional: Generate robots.txt file
+  additionalPaths: async () => {
+    const dynamicPaths = await fetchSlugs();
+    return dynamicPaths;
+  },
+};
