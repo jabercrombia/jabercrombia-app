98b7a235250e08d294f3016f22282d534387b40c
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-04-16 15:31:24 -0500

    github

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,22 @@
 //import { Link } from "gatsby"
 import React from "react"
-
-
+import { Github } from "lucide-react"
+import Link from "next/link"
 
 
 function Footer() {
   return (
-    <footer className="bg-black text-white py-2 text-center w-full mt-5">
+    <footer className="bg-black text-white py-4 text-center w-full mt-5">
         <div className="container mx-auto">
-            <p className="text-xs">Copyright &copy; {new Date().getFullYear()} | <a href="/" className="text-white hover:text-gray-500">jabercrombia</a></p>  
+          <div className="grid grid-cols-2">
+            <div>
+              <Link href="https://www.github.com/jabercrombia" target="_blank"><Github className="text-gray-400 hover:text-white" /></Link>
+            </div>
+            <div>
+            <p className="text-xs text-right">Copyright &copy; {new Date().getFullYear()} | <a href="/" className="text-white hover:text-gray-500">jabercrombia</a></p>  
+            </div>
+          </div>
+            
         </div>
     </footer>
   )
