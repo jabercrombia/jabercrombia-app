88ac537a2544a5fd4daacba2c8d2614be5736561
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-04-15 22:27:04 -0500

    design

--- app/design/[slug]/page.tsx
+++ app/design/[slug]/page.tsx
@@ -14,20 +14,23 @@ dataGraphQL = dataGraphQL?.designCollection.items[0];
 
   return (
 
-    <div className="container mx-auto px-[15px]">
-        <div className="text-center">
-          <h1 className="uppercase mt-[20px] text-3xl">{dataGraphQL?.title}</h1>
-          <p className="text-2xl">{dataGraphQL?.body}</p>
+    <div className="container mx-auto px-[15px] grid grid-cols-1 ">
+        <div>
+          <h1 className="uppercase mt-[20px] text-xl">{dataGraphQL?.title}</h1>
+          <p className="text-xl">{dataGraphQL?.body}</p>
         </div>
-        <div className="grid grid-cols-2 justify-items-center">
-          {dataGraphQL?.designGalleryCollection.items.map(
-            (elem: { title: string; url: string; thumbnail: string }, index: number) => (
-              <div className="p-4" key={index}>
-                <ImageModal imageData={elem}/>
-              </div>
-            )
-          )}
+        <div>
+          <div className="grid grid-cols-1 justify-items-center">
+            {dataGraphQL?.designGalleryCollection.items.map(
+              (elem: { title: string; url: string; thumbnail: string }, index: number) => (
+                <div className="p-4" key={index}>
+                  <ImageModal imageData={elem}/>
+                </div>
+              )
+            )}
+          </div>        
         </div>
+
 
     </div>
   );
