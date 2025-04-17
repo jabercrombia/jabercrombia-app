0c1776135c9b472861a42f6b4fff645dc0d589c5
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-04-17 12:27:08 -0500

    home page sass

--- app/page.tsx
+++ app/page.tsx
@@ -10,63 +10,31 @@ import Image from 'next/image';
 import { getHomepageSections } from "@/lib/api";
 import { CMS_NAME, CMS_URL } from "@/lib/constants";
 import { MoveDown } from "lucide-react";
-
-function HeroPost({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: {
-  title: string;
-  coverImage: any;
-  date: string;
-  excerpt: string;
-  author: any;
-  slug: string;
-}) {
-  return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/posts/${slug}`} className="hover:underline">
-              {title}
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <Date dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {author && <Avatar name={author.name} picture={author.picture} />}
-        </div>
-      </div>
-    </section>
-  );
-}
+import styles from "../components/styles/home.module.scss";
 
 export default async function Page() {
   let homepageSections = await getHomepageSections();
   homepageSections = homepageSections?.pageHeaderCollection?.items;
-
   return (
-    <div>
+    
+    <div className={styles.homePage}>
       <div className="bg-[url(/homepage/sf-banner.jpg)] bg-cover bg-center w-full h-[500px] flex items-center h-screen relative">
-          <div className="container mx-auto px-[15px]">
+          <div className="container mx-auto">
           <h1 className="lg:text-9xl text-[30px] text-center font-thin uppercase text-white opacity-50">Justin Abercrombia</h1>
-          <div className="flex items-center justify-center">
+          <div className="grid grid-cols-1 items-center justify-center place-items-center">
+            <div className="text-white w-full text-center">
+              <p className="my-[10px] font-light">Powered by Contentful CMS with a Nextjs Headless Integration</p>
+            </div>
+            <div>
             <Button className="font-small" aria-label="Explore My Work" variant="outline">
               <Link href="#work" >Explore My Work</Link>
             </Button>
+            </div>
+
           </div>
+
         </div>
-        <MoveDown className="absolute bottom-3 left-1/2 -translate-x-1/2 w-10 h-10 text-white animate-bounce" />
+        <MoveDown />
 
       </div>
       <section id="work"></section>
